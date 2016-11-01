e7f72adc7bca499368f27fbb89da97ac3ff9e8c3
RedSquirrelious <redsquirrelious@gmail.com>
2016-11-01 12:05:50 -0700

    added recipe search controller for optional features later

---
+++ app/helpers/recipe_searches_helper.rb
@@ -0,0 +1,2 @@
+module RecipeSearchesHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,30 @@
 Rails.application.routes.draw do
+  get 'recipe_searches/index'
+
+  get 'recipe_searches/show'
+
+  get 'recipe_searches/new'
+
+  get 'recipe_searches/create'
+
+  get 'recipe_searches/edit'
+
+  get 'recipe_searches/update'
+
+  get 'recipe_searches/destroy'
+
+  get 'recipe_searches/index'
+
+  get 'recipe_searches/show'
+
+  get 'recipe_searches/new'
+
+  get 'recipe_searches/edit'
+
+  get 'recipe_searches/update'
+
+  get 'recipe_searches/destroy'
+
   get 'users/index' => 'users#index'
 
   get 'users/show' => 'users#show'
