79efefccd002e3ca8d1afaf7a48627e47266c939
RedSquirrelious <redsquirrelious@gmail.com>
2016-10-31 21:15:51 -0700

    added constants base_url and auth, renamed id and secret constants, started search class method

--- lib/recipesearchwrapper.rb
+++ lib/recipesearchwrapper.rb
@@ -4,9 +4,35 @@ require 'recipesearch'
 
 class RecipeSearchWrapper
 
-	TOKEN = ENV[APP_ID]
-	SECRET = ENV[APP_KEY]
+#hide that information away!
+	APP_ID = ENV[APP_ID]
+	APP_KEY = ENV[APP_KEY]
 
-	BASE_URL = "https://api.edamam.com/search"
+#this is where it all starts
+	BASE_URL = "https://api.edamam.com/"
+
+#because who wants to type all that?  
+	AUTH = "&app_id=APP_ID&app_key=APP_KEY"
+
+
+#helps users find recipes by an ingredient
+	self.search_recipes_by_one_keyword(keyword)
+		url = BASE_URL + "search?q=#{keyword}" + AUTH
+
+		response = HTTParty.get(url)
+
+		my_recipes = []
+
+		response["recipes"] 
+
+		response["recipes"].each do |recipe|
+			id = recipe["id"]
+			name = recipe["name"]
+
+			my_recipes << RecipeSearch.new(name, id)
+		end
+
+		return my_recipes
+	end
 
 end
