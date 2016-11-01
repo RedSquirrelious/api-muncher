851b1ac0fde4fb6f5eb14e578ebefcff2c6f4585
RedSquirrelious <redsquirrelious@gmail.com>
2016-11-01 11:22:20 -0700

    added recipe search class

--- lib/recipesearch.rb
+++ lib/recipesearch.rb
@@ -3,11 +3,16 @@
 require 'recipesearchwrapper'
 
 class RecipeSearch
-	attr_reader :name, :id
+	attr_reader :name, :id, :photo_url, :recipe_url
 
-	def initialize(name, id)
+	def initialize(name, id, image, url, ingredients, totalnutrients, totaldaily)
 		@name = name
 		@id = id
+		@image = image
+		@url = url
+		@ingredients = ingredients[]
+		@totalnutrients = totalnutrients
+		@totaldaily = totaldaily
 	end
 
 
