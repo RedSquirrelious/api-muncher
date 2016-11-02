0a1d0f9a50eea1ae4e0de84d58c9d2e6a14fba73
RedSquirrelious <redsquirrelious@gmail.com>
2016-11-02 10:20:15 -0700

    simplified initalize

--- lib/recipesearch.rb
+++ lib/recipesearch.rb
@@ -1,19 +1,47 @@
 #recipesearch.rb
 
+#NOTE dietlabels, health labels, cautions, ingredientlines, ingredients, totalNutrients, totalDaily show up as arrays (ingredients is an array of hashes)
+
 require 'recipesearchwrapper'
 
 class RecipeSearch
-	attr_reader :name, :id, :photo_url, :recipe_url
+	attr_reader :label, :id, :photo_url, :recipe_url
 
-	def initialize(name, id, image, url, ingredients, totalnutrients, totaldaily)
-		@name = name
-		@id = id
-		@image = image
-		@url = url
-		@ingredients = ingredients[]
-		@totalnutrients = totalnutrients
-		@totaldaily = totaldaily
+	def initialize(uri, label, params = {})
+		if uri == nil || uri == "" || label == nil || label == ""
+			raise ArguementError
+		end
+
+		@uri = uri   #aka id
+		@label = label
+		@image = params["image"]
+		@shareas = params["shareas"] #link to the recipe on edamam website, as opposed to the source website
+
 	end
 
 
 end
+
+
+
+
+
+
+	# 	@uri = uri   #aka id
+	# 	@label = label
+	# 	@image = params["image"]
+	# 	# @source = recipe["source"]
+	# 	# @sourceicon = recipe["sourceicon"] #
+	# 	# @url = recipe["url"] #link to recipe on source website
+	# 	@shareas = params["shareas"] #link to the recipe on edamam website, as opposed to the source website
+	# 	# @recipe_yield = recipe["recipe_yield"]
+	# 	# @dietabels = recipe["dietLabels"]
+	# 	# @healthabels = recipe["healthlabels"]
+	# 	# @cautions = recipe["cautions"]
+	# 	# @ingredientines = recipe["ingredientlines"]
+	# 	# @ingredients = recipe["ingredients"]
+	# 	# @calories = recipe["calories"]
+	# 	# @totalweight = recipe["totalweight"]
+	# 	# @totalnutrients = recipe["totalnutrients"]
+	# 	# @totaldaily = recipe["totaldaily"]
+	# end
