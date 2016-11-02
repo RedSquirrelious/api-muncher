c56328bfaf436f0fafb006cb259f0d60da83b5e5
RedSquirrelious <redsquirrelious@gmail.com>
2016-11-02 10:22:10 -0700

    add tests

--- test/lib/recipesearchwrapper_test.rb
+++ test/lib/recipesearchwrapper_test.rb
@@ -3,4 +3,54 @@ require 'recipesearchwrapper'
 
 class RecipeSearchWrapperTest < ActionController::TestCase
 
+#make sure rake is picking up the test
+	test "the truth" do
+		assert true
+	end
+
+	test "Can Retrieve a List of Recipes" do
+		VCR.use_cassette("recipe list") do 
+			
+			recipes = RecipeSearchWrapper.search_by_one_keyword("carrot")
+
+			assert recipes.length > 0
+			assert recipes.is_a? Array
+			assert_not recipes.is_empty?
+
+			recipes.each do |recipe|
+
+end 
+
+	test "Searches without a Keyword Fail" do
+		VCR.use_cassette("no-keyword") do
+			recipes = RecipeSearchWrapper.search_by_one_keyword("")
+
+			assert recipes == nil
+		end
+	end
+
+
+	test "Retrieves Nil When the App ID is Wrong" do 
+		VCR.use_cassette("bad-token") do
+			recipes = RecipeSearchWrapper.search_by_one_keyword("carrot", "bad-token")
+
+			assert recipes == Error 500
+		end
+	end 
+
+		test "Retrieves Nil When the App Key is Wrong" do 
+		VCR.use_cassette("bad-key") do
+			recipes = RecipeSearchWrapper.search_by_one_keyword("carrot", "bad-token")
+
+			assert recipes == nil
+		end
+	end 
+
+	test "Retrieves Nil When the Keyword is not an Option" do 
+		VCR.use_cassette("bad-keyword") do
+			recipes = RecipeSearchWrapper.search_by_one_keyword("grok")
+
+			assert recipes == nil
+		end
+	end 
 end
