81b7e0e17839fe3780494c72fbfbdeadb0a911c8
RedSquirrelious <redsquirrelious@gmail.com>
2016-11-03 14:14:39 -0700

    added search to show method

--- app/controllers/recipe_searches_controller.rb
+++ app/controllers/recipe_searches_controller.rb
@@ -2,11 +2,13 @@ class RecipeSearchesController < ApplicationController
   def index
   end
 
-  def show
+  def show(keyword, my_app_id = nil, my_app_key = nil, list_start = 0, list_end = 10)
+  	@search = RecipeSearchWrapper.search_by_one_keyword(keyword, my_app_id = nil, my_app_key = nil)
+  	return @search
   end
 
   def new
-  	
+  	@search = RecipeSearchWrapper.new
   end
 
   def create
