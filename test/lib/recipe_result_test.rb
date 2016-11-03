59942a4c1dcb68ef43c51da12b090e781bb27faf
RedSquirrelious <redsquirrelious@gmail.com>
2016-11-02 17:02:17 -0700

    added tests

--- test/lib/recipe_result_test.rb
+++ test/lib/recipe_result_test.rb
@@ -7,4 +7,41 @@ class RecipeResultTest < ActionController::TestCase
 		assert true
 	end
 
+	test 'Must Provide a URI and a Label for a Recipe Result' do
+	
+	assert_raises ArgumentError do
+		RecipeResult.new nil, nil
+	end
+
+   assert_raises ArgumentError do
+     RecipeResult.new "", ""
+   end
+
+   assert_raises ArgumentError do
+     RecipeResult.new "", "SomeLabel"
+   end
+
+   assert_raises ArgumentError do
+     RecipeResult.new "SomeURI", ""
+   end
+
+   assert_raises ArgumentError do
+     RecipeResult.new "SomeURI", nil
+   end
+
+   assert_raises ArgumentError do
+     RecipeResult.new nil, "SomeLabel"
+   end
+ end
+
+ test "Name Attribute is Set Correctly" do
+   test_me = RecipeResult.new "SomeURI", "SomeLabel"
+   assert test_me.label == "SomeLabel"
+ end
+
+ test "ID Attribute is Set Correctly" do
+   test_me = RecipeResult.new "SomeURI", "SomeLabel"
+   assert test_me.uri == "SomeURI"
+ end
+
 end
