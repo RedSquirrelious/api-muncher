c44479f349ff3d045ec0949997fe5fe0813fb4d0
RedSquirrelious <redsquirrelious@gmail.com>
2016-11-04 08:52:07 -0700

    tweaked search method, wrote notes about show method

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,11 @@
+
 class RecipesController < ApplicationController
-  def search
+
 
   def search
-  	@search = RecipeSearchWrapper.search_by_one_keyword(params[:query], list_start = 0, list_end = 10)
-  	redirect_to root_path and return @search
+  	@results = RecipeSearchWrapper.search_by_one_keyword(params[:query])
+  	# binding.pry
+  	return render :index
   end
   	  # def self.search(keyword, my_app_id = nil, my_app_key = nil, list_start = 0, list_end = 10)
   # 	@search = RecipeSearchWrapper.search_by_one_keyword(keyword, my_app_id = nil, my_app_key = nil)
@@ -14,13 +16,27 @@ class RecipesController < ApplicationController
 #    # if product query matches a product(s)
 #    # render those image(s), if any match.
 #    render :index
-  end
+  # end
 
   def index
 
   end
 
+  def flip_page
+
+  end
+
   def show
+  	#paginate with @hit in the list_start local?
+  	@result = RecipeSearchWrapper.search_by_one_keyword(params["search_term"], params["hit_num"].to_i, params["hit_num"].to_i)
+  	@uri = params["uri"]   #aka id
+		@label = params["label"]
+		@image = params["image"]
+		@url = params["url"]
+		@shareas = params["shareas"] #link to the recipe on edamam website, as opposed to the source website
+		@ingredientsLines = params["ingredientsLines"]
+		@calories = params["calories"]
+		@totalNutrients = params["totalNutrients"]
   end
 
   def new
